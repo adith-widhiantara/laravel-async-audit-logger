4d0aa18f5ebeb698662890d63e218f70feccb173
Adith Widhiantara <adithwidhiantara@gmail.com>
2026-01-13 15:46:55 +0700

    Initiate unit test

---
+++ tests/TestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use Adithwidhiantara\Audit\AuditingServiceProvider;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            AuditingServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
+        $migration->up();
+        */
+    }
+}
